9221d14854cf8e83d04c7307b4885db95b94d04e
ali lotfi <a.lotfi@pio.ir>
2023-01-24 19:01:47 +0330

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	apiKey := viper.GetString("API_KEY")
+	if apiKey == "" {
+		panic("Missing API KEY")
+	}
+
+
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+
+	// get user prompt all in one line
+	fmt.Println("Enter your prompt:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	prompt := scanner.Text()
+
+
+	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			prompt,
+		},
+		MaxTokens:   gpt3.IntPtr(100),
+		Temperature: gpt3.Float32Ptr(0),
+	}, func(resp *gpt3.CompletionResponse) {
+		fmt.Print(resp.Choices[0].Text)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
+	fmt.Printf("\n")
+}
